1b7a74f68fc1c89716ea3c870384fe5e241529f8
Duberly2004 <ivanmondragonmanchay@gmail.com>
2023-08-01 11:46:07 -0500

    Agregación de la ruta vase en vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://Duberly2004.github.io/wortika-frontend/'
+  base: 'https://duberlymondragon.com/'
 })
