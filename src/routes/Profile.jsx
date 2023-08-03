84f4640c3605f50372208a525169454067cdeac5
Duberly2004 <ivanmondragonmanchay@gmail.com>
2023-08-02 20:37:32 -0500

    Solucion de errores

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -3,15 +3,20 @@ import { useAuth } from '../context/auth/AuthContext'
 import { Navigate, Outlet, useNavigate } from 'react-router-dom';
 // Este componente aun no esta en uso
 export default function Profile() {
-   const navigate = useNavigate()
+  //  const navigate = useNavigate()
     const {user,isLoading} = useAuth()
-    console.log(user)
-    useEffect(()=> {
-      if(user){        
-        console.log(user,isLoading)
-        if(user.profile_name === 'company') return navigate('/profile-company')
-        // if(user.profile_name === 'company') return <Navigate to='/profile-company'/>
-        if(user.profile_name === 'candidate') return navigate('/profile-candidate')
-      } 
-    },[])
+  //   console.log(user)
+  //   useEffect(()=> {
+  //     if(user){        
+  //       console.log(user,isLoading)
+  //       if(user.profile_name === 'company') return navigate('/profile-company')
+  //       // if(user.profile_name === 'company') return <Navigate to='/profile-company'/>
+  //       if(user.profile_name === 'candidate') return navigate('/profile-candidate')
+  //     } 
+  //   },[])+
+  
+  console.log(user)
+  return (
+    <h1>Profile</h1>
+  )
 }
