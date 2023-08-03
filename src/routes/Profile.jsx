881294402ad1010955eae02405aefe50bcf97327
Duberly2004 <ivanmondragonmanchay@gmail.com>
2023-08-03 10:24:15 -0500

    Creacion del la ruta porofile

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,21 +1,6 @@
-import React, { useEffect } from 'react';
-import { useAuth } from '../context/auth/AuthContext'
-import { Navigate, Outlet, useNavigate } from 'react-router-dom';
-// Este componente aun no esta en uso
+import React from 'react';
 export default function Profile() {
-  //  const navigate = useNavigate()
-    const {user,isLoading} = useAuth()
-  //   console.log(user)
-  //   useEffect(()=> {
-  //     if(user){        
-  //       console.log(user,isLoading)
-  //       if(user.profile_name === 'company') return navigate('/profile-company')
-  //       // if(user.profile_name === 'company') return <Navigate to='/profile-company'/>
-  //       if(user.profile_name === 'candidate') return navigate('/profile-candidate')
-  //     } 
-  //   },[])+
-  
-  console.log(user)
+
   return (
     <h1>Profile</h1>
   )
