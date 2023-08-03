043d935376dfcfcafb6f25b12de2a5ac8ee0bc17
Duberly2004 <ivanmondragonmanchay@gmail.com>
2023-08-02 19:06:43 -0500

    Configuración de la rurta base en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://64cae5d9c345110f2f7fa576--wortika.netlify.app/' //base
+  base: 'https://wortika.netlify.app/' //base
 })
